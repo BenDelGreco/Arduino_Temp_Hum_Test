f5a9e1720f548f03bcd81d20203701f8b1fde333
Ben Del Greco <benedettodelgreco@gmail.com>
2019-03-23 15:22:28 +0100

    Added 7-seg display and libraries to Simple Temp and Hum Test

---
+++ test_Temp_Hum.ino
@@ -0,0 +1,57 @@
+#include "dht.h"
+#include "TM1637.h"
+
+//{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+//0~9,A,b,C,d,E,F
+ 
+
+ 
+#define dht_pin 2 // Pin sensor is connected to
+#define CLK 3//Pins for TM1637       
+#define DIO 4
+ 
+TM1637 tm1637(CLK,DIO);
+dht DHT;
+ 
+
+void setup(){
+  
+  tm1637.init();
+  tm1637.set(BRIGHT_TYPICAL); 
+  //BRIGHT_TYPICAL = 2,BRIGHT_DARKEST = 0,BRIGHTEST = 7;
+ 
+  delay(1500);//Delay to let system boot
+
+ 
+}
+ 
+
+ 
+void loop(){
+ 
+ 
+  DHT.read11(dht_pin);
+ 
+
+ int temp = DHT.temperature;
+ int humidity = DHT.humidity;
+ 
+ int digitoneT = temp / 10;
+ int digittwoT = temp % 10;
+ 
+ int digitoneH = humidity / 10;
+ int digittwoH = humidity % 10;
+  
+
+    tm1637.display(1,digitoneT); 
+    tm1637.display(2,digittwoT);
+    tm1637.display(3,12);  //Scrive C
+    delay (3000);
+    
+    tm1637.display(1,23); 
+    tm1637.display(2,digitoneH); 
+    tm1637.display(3,digittwoH);
+
+
+    delay(3000);
+}
